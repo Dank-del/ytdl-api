ea6c6c34dc9e0ecb9e6cfe7aef7ac8c0326ba0d9
Sayan Biswas <sayan@pokurt.me>
2022-12-16 01:02:22 +0530

    use ssyoutube api
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- routes/download.py
+++ routes/download.py
@@ -6,8 +6,11 @@ router = APIRouter()
 
 @router.get("/video", tags=["download"])
 async def download_video(video_url: str):
-    ydl_opts = {"outtmpl": "%(id)s.%(ext)s", "progress_hooks": [
-        show_download_progress]}
+    ydl_opts = {
+        "outtmpl": "%(id)s.%(ext)s", 
+        "progress_hooks": [show_download_progress],
+        "format": "bestvideo+bestaudio"
+    }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         return ydl.extract_info(video_url, download=False)
         # ydl.download([video_url])
@@ -18,6 +21,26 @@ async def download_video(video_url: str):
         # return responses.StreamingResponse(
         #     iterFile(), media_type="video/mp4"
         # )
+
+@router.get("/ssyoutube", tags=["download"])
+async def download_ss_youtube(video_url: str):
+    import http.client
+    import json
+    conn = http.client.HTTPSConnection("ssyoutube.com")
+
+    headersList = {
+        "Accept": "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+        "Content-Type": "application/json" 
+    }
+    
+    
+    payload = json.dumps({ "url": video_url })
+
+    conn.request("POST", "/api/convert", payload, headersList) # type: ignore    
+    response = conn.getresponse()
+    result = response.read()
+    return json.loads(result)
 
 
 def show_download_progress(d):
